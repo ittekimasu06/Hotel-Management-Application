0bb7c3c63024941a956e0c0736e895c041a42380
Doanh <soycreeper06@gmail.com>
2025-03-26 17:24:40 +0700

    deploy til dead

---
+++ update-database.ps1
@@ -0,0 +1,7 @@
+﻿Write-Host "Running EF6 Update-Database"
+& "$(Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\MSBuild\ToolsVersions\*').MSBuildToolsPath\MSBuild.exe" `
+    .\QuanLyKhachSan.csproj `
+    /t:update-database `
+    /p:Configuration=Release `
+    /p:ConnectionStrings__DefaultConnection="Data source=(localDB)\MSSQlLocalDB;AttachDbFilename=|DataDirectory|\QuanLyKhachSan.mdf;Initial Catalog=DefaultConnection;Integrated Security=True"  `
+    /verbosity:normal
